32ad9da2b2603cefd4c2bbee233db2c84de9ea24
HARSHITA PRIYA <harshitapriyaph5@gmail.com>
2023-01-24 20:19:19 +0100

    final commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const io = new Server(server);
 
 io.on("connection", (socket) => {
   socket.on("message", (message) => {
-    console.log("A new message", message);
+    io.emit("message", message);
   });
 });
 
